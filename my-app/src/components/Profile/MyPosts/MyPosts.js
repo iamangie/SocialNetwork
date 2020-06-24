f30a8217711179a1908f325225f0a90ac7a44c3f
iamangie <angie.diachuk@gmail.com>
2020-06-24 15:00:39 +0300

    React.memo

--- my-app/src/components/Profile/MyPosts/MyPosts.js
+++ my-app/src/components/Profile/MyPosts/MyPosts.js
@@ -3,37 +3,25 @@ import Post from "./Post/Post";
 import style from "./MyPosts.module.css";
 import AddNewPostForm from "./AddNewPostForm/AddNewPostForm";
 
-class MyPosts extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    debugger;
-    if (this.props.addPost !== nextProps.addPost) {
-      return true;
-    }
-    if (this.props.posts !== nextProps.posts) {
-      return true;
-    }
-    return false;
-  }
-  render() {
-    console.log("RENDER");
-    console.log(this.props);
+const MyPosts = React.memo((props) => {
+  console.log("RENDER");
+  console.log(props);
 
-    const postsElements = this.props.posts.map((p) => (
-      <Post message={p.message} like={p.likesCount} id={p.id} />
-    ));
+  const postsElements = props.posts.map((p) => (
+    <Post message={p.message} like={p.likesCount} id={p.id} />
+  ));
 
-    const onAddPost = (values) => {
-      this.props.addPost(values.newPostText);
-    };
+  const onAddPost = (values) => {
+    props.addPost(values.newPostText);
+  };
 
-    return (
-      <div className={style.postsBlock}>
-        <h3>My posts</h3>
-        <AddNewPostForm onSubmit={onAddPost} />
-        <div className={style.posts}>{postsElements}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={style.postsBlock}>
+      <h3>My posts</h3>
+      <AddNewPostForm onSubmit={onAddPost} />
+      <div className={style.posts}>{postsElements}</div>
+    </div>
+  );
+});
 
 export default MyPosts;
