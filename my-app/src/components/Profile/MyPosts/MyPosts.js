fa944f69417514ff39c9c64e3abf9dff4ce3955d
iamangie <angie.diachuk@gmail.com>
2020-06-24 14:10:38 +0300

    shouldComponentUpdate()

--- my-app/src/components/Profile/MyPosts/MyPosts.js
+++ my-app/src/components/Profile/MyPosts/MyPosts.js
@@ -3,22 +3,37 @@ import Post from "./Post/Post";
 import style from "./MyPosts.module.css";
 import AddNewPostForm from "./AddNewPostForm/AddNewPostForm";
 
-const MyPosts = (props) => {
-  const postsElements = props.posts.map((p) => (
-    <Post message={p.message} like={p.likesCount} id={p.id} />
-  ));
+class MyPosts extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    debugger;
+    if (this.props.addPost !== nextProps.addPost) {
+      return true;
+    }
+    if (this.props.posts !== nextProps.posts) {
+      return true;
+    }
+    return false;
+  }
+  render() {
+    console.log("RENDER");
+    console.log(this.props);
 
-  const onAddPost = (values) => {
-    props.addPost(values.newPostText);
-  };
+    const postsElements = this.props.posts.map((p) => (
+      <Post message={p.message} like={p.likesCount} id={p.id} />
+    ));
 
-  return (
-    <div className={style.postsBlock}>
-      <h3>My posts</h3>
-      <AddNewPostForm onSubmit={onAddPost} />
-      <div className={style.posts}>{postsElements}</div>
-    </div>
-  );
-};
+    const onAddPost = (values) => {
+      this.props.addPost(values.newPostText);
+    };
+
+    return (
+      <div className={style.postsBlock}>
+        <h3>My posts</h3>
+        <AddNewPostForm onSubmit={onAddPost} />
+        <div className={style.posts}>{postsElements}</div>
+      </div>
+    );
+  }
+}
 
 export default MyPosts;
